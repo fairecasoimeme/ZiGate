dec819263d4736cd3d4b492bbf70c6ceed8f565b
ISO-B <3048685+ISO-B@users.noreply.github.com>
2019-07-09 12:33:32 +0300

    Added IAS WD Squawk command 0x0112
    0x0112 Write Attribute request IAS_WD Squawk
     <address mode: uint8_t>
     <target short address: uint16_t>
     <source endpoint: uint8_t>
     <destination endpoint: uint8_t>
     <direction: uint8_t>
     <manufacturer specific: uint8_t>
     <manufacturer id: uint16_t>
     <SquawkModeStrobeAndLevel: uint8_t>

--- Module Radio/Firmware/src/ZiGate/Source/ZigbeeNodeControlBridge/SerialLink.h
+++ Module Radio/Firmware/src/ZiGate/Source/ZigbeeNodeControlBridge/SerialLink.h
@@ -284,6 +284,7 @@ typedef enum
     E_SL_MSG_WRITE_ATTRIBUTE_REQUEST                            =  0x0110,
     E_SL_MSG_WRITE_ATTRIBUTE_RESPONSE                           =  0x8110,
     E_SL_MSG_WRITE_ATTRIBUTE_REQUEST_IAS_WD                     =  0x0111,
+    E_SL_MSG_WRITE_ATTRIBUTE_REQUEST_IAS_WD_SQUAWK              =  0x0112,
     E_SL_MSG_CONFIG_REPORTING_REQUEST                           =  0x0120,
     E_SL_MSG_CONFIG_REPORTING_RESPONSE                          =  0x8120,
     E_SL_MSG_REPORT_ATTRIBUTES                                  =  0x8121,
