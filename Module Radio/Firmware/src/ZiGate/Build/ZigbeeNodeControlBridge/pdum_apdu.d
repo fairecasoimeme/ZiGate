33c6672e164e679a4f85c4eda492f2edf293cdf3
jonathan.thompson <jonathan@urbaneinnovation.com>
2019-08-08 11:10:31 -0600

    Removed coordinator build artifacts. Added .gitignore to exclude future build artifacts.

--- Module Radio/Firmware/src/ZiGate/Build/ZigbeeNodeControlBridge/pdum_apdu.d
+++
@@ -1,2 +0,0 @@
-pdum_apdu.o: \
- c:/NXP/bstudio_nxp/workspace/JN-AN-1216-Zigbee-3-0-IoT-ControlBridge-v1011/Source/ZigbeeNodeControlBridge/pdum_apdu.S
--- Module Radio/Firmware/src/ZiGate/Build/ZigbeeNodeControlBridge/portasm_JN516x.d
+++
@@ -1,2 +0,0 @@
-portasm_JN516x.o: \
- c:/NXP/bstudio_nxp/sdk/JN-SW-4170/Components/ZigbeeCommon/Source/portasm_JN516x.S
