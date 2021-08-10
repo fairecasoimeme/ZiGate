1c156d61e53def21cde7bc64b03f05d775381178
fairecasoimeme <akila@faire-ca-soi-meme.fr>
2021-08-10 09:46:07 +0200

    version update

--- ModuleRadio/Firmware/src/ZiGate/Source/ZigbeeNodeControlBridge/app_Znc_cmds.c
+++ ModuleRadio/Firmware/src/ZiGate/Source/ZigbeeNodeControlBridge/app_Znc_cmds.c
@@ -100,7 +100,7 @@
 #endif
 
 #ifndef VERSION
-#define VERSION    0x00040320
+#define VERSION    0x00030320
 #endif
 /****************************************************************************/
 /***    Type Definitions                          ***/
--- ModuleRadio/Firmware/src/ZiGate/Source/ZigbeeNodeControlBridge/app_start.c
+++ ModuleRadio/Firmware/src/ZiGate/Source/ZigbeeNodeControlBridge/app_start.c
@@ -114,7 +114,7 @@
 #endif
 
 #ifndef VERSION
-#define VERSION    0x00040320
+#define VERSION    0x00030320
 #endif
 
 #ifndef TRACE_EXC
